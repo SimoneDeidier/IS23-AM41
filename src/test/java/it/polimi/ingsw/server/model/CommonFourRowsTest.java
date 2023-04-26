8e2f6957b551058f2cad10fd163b50ad215fe795
IacopoErmacoraPolimi <126016574+IacopoErmacoraPolimi@users.noreply.github.com>
2023-04-26 20:27:29 +0200

    Update CommonFourRowsTest

--- src/test/java/it/polimi/ingsw/server/model/CommonFourRowsTest.java
+++ src/test/java/it/polimi/ingsw/server/model/CommonFourRowsTest.java
@@ -32,12 +32,12 @@ class CommonFourRowsTest {
         // create a sample shelf with less than four rows of one, two or three different types
         Shelf shelfB = new Shelf();
 
-        shelfB.insertItems(0, Arrays.asList(new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.BLUE),       new Item(ItemColor.PINK),       new Item(ItemColor.BLUE),       new Item(ItemColor.GREEN)));
-        shelfB.insertItems(1, Arrays.asList(new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.WHITE),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.BLUE),       new Item(ItemColor.WHITE),      new Item(ItemColor.WHITE)));
-        shelfB.insertItems(2, Arrays.asList(new Item(ItemColor.PINK),       new Item(ItemColor.PINK),       new Item(ItemColor.BLUE),       new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.PINK)));
-        shelfB.insertItems(3, Arrays.asList(new Item(ItemColor.YELLOW),     new Item(ItemColor.BLUE),       new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.YELLOW),     new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.YELLOW)));
-        shelfB.insertItems(4, Arrays.asList(new Item(ItemColor.GREEN),      new Item(ItemColor.BLUE),       new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.GREEN),      new Item(ItemColor.GREEN)));
+        shelfB.insertItems(0, Arrays.asList(new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.BLUE),       new Item(ItemColor.PINK),       new Item(ItemColor.YELLOW),       new Item(ItemColor.GREEN)));
+        shelfB.insertItems(1, Arrays.asList(new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.WHITE),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.BLUE),       new Item(ItemColor.YELLOW),      new Item(ItemColor.GREEN)));
+        shelfB.insertItems(2, Arrays.asList(new Item(ItemColor.YELLOW),       new Item(ItemColor.PINK),       new Item(ItemColor.BLUE),       new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.PINK)));
+        shelfB.insertItems(3, Arrays.asList(new Item(ItemColor.YELLOW),     new Item(ItemColor.BLUE),       new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.YELLOW),     new Item(ItemColor.GREEN), new Item(ItemColor.PINK)));
+        shelfB.insertItems(4, Arrays.asList(new Item(ItemColor.GREEN),      new Item(ItemColor.BLUE),       new Item(ItemColor.GREEN),      new Item(ItemColor.LIGHT_BLUE), new Item(ItemColor.YELLOW),      new Item(ItemColor.PINK)));
 
-        assertTrue(commonFourRows.check(shelfB));
+        assertFalse(commonFourRows.check(shelfB));
     }
 }
