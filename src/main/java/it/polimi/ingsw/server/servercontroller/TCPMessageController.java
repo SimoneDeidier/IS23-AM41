59526c7c7c75a9c4b987fd2b97cebebaada546c1
Simone Deidier <simone.deidier@mail.polimi.it>
2023-05-02 12:14:43 +0200

    Some examples of tcp

--- src/main/java/it/polimi/ingsw/server/servercontroller/TCPMessageController.java
+++ src/main/java/it/polimi/ingsw/server/servercontroller/TCPMessageController.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw.server.servercontroller;
 
+import it.polimi.ingsw.server.model.Player;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +21,28 @@ public class TCPMessageController {
         switch (header) {
             case "Helo" -> {
                 list = new ArrayList<>();
-                list.add(new TCPMessage("Connected", null));
+                if(gameController.getAvailableSlot() == -1 || gameController.getAvailableSlot() > 0) {
+                    // primo giocatore o ci sono slot per altri giocatori
+                    list.add(new TCPMessage("Connected", null));
+                }
+                else {
+                    // la lobby è piena
+                    list.add(new TCPMessage("Goodbye", null));
+                }
+                return list;
+            }
+            case "Presentation" -> {
+                list = new ArrayList<>();
+                if(gameController.getAvailableSlot() == -1) {
+                    // aggiunge questo giocatore
+                    Player player = new Player(message.getBody().getPlayerNickname());
+                    gameController.addPlayer();
+                    list.add(new TCPMessage("Get Parameters", null));
+                }
+                else if(gameController.getAvailableSlot() > 0) {
+                    // check il nickname non è già usato da un altro giocatore
+                    // ...
+                }
                 return list;
             }
             case "Disconnect" -> {
