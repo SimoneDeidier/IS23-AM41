e9656b20b4f03df1bab28b275d218194682e8e3a
Simone Deidier <simone.deidier@mail.polimi.it>
2023-05-21 21:54:42 +0200

    Commons okay on gui

--- src/main/java/it/polimi/ingsw/client/view/controllers/CommonGoalController.java
+++ src/main/java/it/polimi/ingsw/client/view/controllers/CommonGoalController.java
@@ -16,6 +16,12 @@ public class CommonGoalController {
     private static final double HEIGHT_ONE = 220.0;
     private static final double OFFSET_X_ONE = 153.0;
     private static final double OFFSET_Y_ONE = 69.0;
+    private static final double WIDTH_TWO = 274.0;
+    private static final double HEIGHT_TWO = 181.0;
+    private static final double OFFSET_Y_TWO = 81.0;
+    private static final double OFFSET_X_TWO_LEFT = 31.0;
+    private static final double OFFSET_X_TWO_RIGHT = 335.0;
+
 
     public void setCommons(boolean isOneCommon, List<Image> images) {
         if(isOneCommon) {
@@ -23,8 +29,22 @@ public class CommonGoalController {
             imageView.setFitWidth(WIDTH_ONE);
             imageView.setFitHeight(HEIGHT_ONE);
             imageView.setTranslateX(OFFSET_X_ONE);
-            imageView.setTranslateX(OFFSET_Y_ONE);
+            imageView.setTranslateY(OFFSET_Y_ONE);
             mainAnchorPane.getChildren().add(imageView);
+        }
+        else {
+            ImageView imageView1 = new ImageView(images.get(0));
+            imageView1.setFitWidth(WIDTH_TWO);
+            imageView1.setFitHeight(HEIGHT_TWO);
+            imageView1.setTranslateY(OFFSET_Y_TWO);
+            imageView1.setTranslateX(OFFSET_X_TWO_LEFT);
+            mainAnchorPane.getChildren().add(imageView1);
+            ImageView imageView2 = new ImageView(images.get(1));
+            imageView2.setFitWidth(WIDTH_TWO);
+            imageView2.setFitHeight(HEIGHT_TWO);
+            imageView2.setTranslateY(OFFSET_Y_TWO);
+            imageView2.setTranslateX(OFFSET_X_TWO_RIGHT);
+            mainAnchorPane.getChildren().add(imageView2);
         }
     }
 
