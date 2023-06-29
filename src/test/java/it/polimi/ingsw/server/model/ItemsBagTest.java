c348151576ced1c71ba2448c7100eea182b6988a
IacopoErmacoraPolimi <126016574+IacopoErmacoraPolimi@users.noreply.github.com>
2023-06-29 23:33:05 +0200

    Javadoc ItemsBagTest.java

--- src/test/java/it/polimi/ingsw/server/model/ItemsBagTest.java
+++ src/test/java/it/polimi/ingsw/server/model/ItemsBagTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+* Class to test the class ItemsBag
+*/
 class ItemsBagTest {
 
     private final static int NUMBEROFITEM = 132;
@@ -19,13 +22,18 @@ class ItemsBagTest {
     itemsBag.resetItemsBag();
     }
 
-
+/**
+* Checks if the bag gets correctly setup by the setupBag method
+*/
     @Test
     void testSetupBag() {
         itemsBag.setupBag();
         assertEquals(NUMBEROFITEM,itemsBag.getRemainingItemsCount());
     }
 
+/**
+* Checks if the remaining items get counted correctly
+*/
     @Test
     void testGetRemainingItemsCount() {
         assertEquals(ZERO,itemsBag.getRemainingItemsCount());
@@ -33,6 +41,9 @@ class ItemsBagTest {
         assertEquals(NUMBEROFITEM,itemsBag.getRemainingItemsCount());
     }
 
+/**
+* Tests the pickItem method
+*/
     @Test
     void testPickItem() {
         itemsBag.setupBag();
