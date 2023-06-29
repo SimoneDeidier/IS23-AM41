e914fbc9088d4f32ee6cc696b951e4198c03284b
IacopoErmacoraPolimi <126016574+IacopoErmacoraPolimi@users.noreply.github.com>
2023-06-29 23:14:36 +0200

    Javadoc ScoringTokenTest.java

--- src/test/java/it/polimi/ingsw/server/model/ScoringTokenTest.java
+++ src/test/java/it/polimi/ingsw/server/model/ScoringTokenTest.java
@@ -3,14 +3,23 @@ package it.polimi.ingsw.server.model;
 import it.polimi.ingsw.server.model.tokens.ScoringToken;
 import org.junit.jupiter.api.Test;
 
+/**
+* Test class for the scoring toke 
+*/
 public class ScoringTokenTest {
 
+/**
+* Checks if the isTakeable method is returning true on a takeable token
+*/
     @Test
     public void testIsTakeable() {
         ScoringToken token=new ScoringToken(5);
         assert(token.isTakeable());
     }
 
+/**
+* Checks if the isTakeable method is returning false on a not takeable token
+*/
     @Test
     public void testIsNotTakeable() {
         ScoringToken token=new ScoringToken(5);
